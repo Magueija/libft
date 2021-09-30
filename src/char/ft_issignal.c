dae07bd6aeb20aeae5aff217231c0aeeb7be17ad
tosilva <tosilva@student.42lisboa.com>
2021-09-30 13:39:25 +0100

    ft_issignal comment added

--- src/char/ft_issignal.c
+++ src/char/ft_issignal.c
@@ -6,10 +6,16 @@
 /*   By: tosilva <tosilva@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 13:33:59 by tosilva           #+#    #+#             */
-/*   Updated: 2021/09/30 13:34:40 by tosilva          ###   ########.fr       */
+/*   Updated: 2021/09/30 13:38:56 by tosilva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+** Checks if 'c' is a plus(+) or a minus(-) signal.
+** @param:	- [int] char to be checked
+** @return:	[int] non 0 if signal, 0 if not
+*/
+i
 int	ft_issignal(char c)
 {
 	return (c == '+' || c == '-');
